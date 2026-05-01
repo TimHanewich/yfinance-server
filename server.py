121e27c451fcb8afa4fdb69f337e79e84fd25bff
Tim Hanewich <tahanewich@live.com>
2026-05-01 08:56:13 -0400

    Updated server to handle multiple

--- server.py
+++ server.py
@@ -38,41 +38,45 @@ def quote(symbol:str):
     if request.method.upper() == "GET":
         print("Request for quote data for '" + symbol.upper() + "'")
 
-        # pull down data with yfinance
+        # Split into individual symbols (they may have provided both)
+        symbols:list[str] = symbol.upper().split(",")
+        print(str(symbols))
+
+        # pull down data for all
         printnr("Pulling data using yfinance... ")
-        ticker = yf.Ticker(symbol.upper())
-        data = ticker.history(period="2d")
-            
-        # Do we have legit data?
-        # yfinance will return just an empty data array if it didnt work (i.e. bogus symbol)
-        if data.empty:
-            r = Response()
-            r.status = 404
-            r.headers["Content-Type"] = "text/plain"
-            r.set_data("Data pull from yfinance did not result in any data. Are you sure you provided a valid ticker?")
-            return r
-
-        # Extract current price
-        printnr("Extracting current price... ")
-        current_price:float = data["Close"].iloc[-1]
+        data = yf.download(symbols, period="2d", group_by="ticker")
         print("done")
 
-        # Extract previous close
-        printnr("Extracting previous close... ")
-        if len(data) < 2: # if our request for 2 days of data only returned 1 day of data, that means it is an IPO (rare). So there isn't a "previous close" to compare to. So in that case, just do the changes against the opening price.
-            prev_close = data["Open"].iloc[-1]
-        else: # there was data for a day before yesterday... not an IPO. most common obviously.
-            prev_close:float = data["Close"].iloc[-2]
-        print("done")
+        # loop through each to extract data, construct body to return
+        ToReturn:dict = {}
+        for symb in symbols:
+            ThisSymbData = data[symb.upper()]
+            if ThisSymbData.isna().all().all(): # if it is all NaN data (not valid symbol probably)
+                pass
+            else:
 
-        # Calculate
-        dollar_change:float = current_price - prev_close
-        percent_change:float = dollar_change / prev_close
+                # Extract current price
+                printnr("Extracting current price... ")
+                current_price:float = ThisSymbData["Close"].iloc[-1]
+                print("done")
 
-        # Construct
-        # Percent change is sent already pre-multiplied by 100
-        ToReturn:dict = {"price": round(current_price, 2), "change": round(dollar_change, 2), "changePercent": round(percent_change*100, 1)}
+                # Extract previous close
+                printnr("Extracting previous close... ")
+                if len(ThisSymbData) < 2: # if our request for 2 days of data only returned 1 day of data, that means it is an IPO (rare). So there isn't a "previous close" to compare to. So in that case, just do the changes against the opening price.
+                    prev_close = ThisSymbData["Open"].iloc[-1]
+                else: # there was data for a day before yesterday... not an IPO. most common obviously.
+                    prev_close:float = ThisSymbData["Close"].iloc[-2]
+                print("done")
 
+                # Calculate
+                dollar_change:float = current_price - prev_close
+                percent_change:float = dollar_change / prev_close
+
+                # Construct
+                # Percent change is sent already pre-multiplied by 100
+                ToReturn[symb.upper()] = {"price": round(current_price, 2), "change": round(dollar_change, 2), "changePercent": round(percent_change*100, 1)}
+
+        
         # return
         print("Returning...")
         r = Response()
